91f522551881bf7e006e30a2ea2edd7bcbd02f5c
Bhavyashree-DC <bhavyadc05@gmail.com>
2024-01-22 22:53:01 +0530

    added Input form component

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,10 +1,47 @@
-vue <template>
+ <template>
+  <div class="container">
+    <div class="todo-app">
+        <header>
+            <div class="header">
+                <h2>{{ header }} </h2>  
+            </div>
+        </header>
+        <hr>
+        <div class="date">
+            {{ currentDate }}
+        </div>
+        <InputForm></InputForm>
   
+    </div>
+</div>
 </template>
 
 <script>
+import  InputForm  from './components/InputForm.vue'
 export default {
+  name:'App',
+  components:{ 
+      InputForm,
+  },
 
+  data(){
+     return{
+        header:'ToDo App', 
+        dateFormatOptions: {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+     },
+     }
+  },
+  
+  computed:{
+     currentDate(){
+        let today = new Date();
+        let dateFormat = new Intl.DateTimeFormat("en-GB", this.dateFormatOptions);
+        return dateFormat.format(today);
+     },
+}
 }
 </script>
 
