189c35a7112e058c3b0ec7107ca7be07565a3b4a
Bhavyashree-DC <bhavyadc05@gmail.com>
2024-01-22 22:54:36 +0530

    Added input field & style

---
+++ todo-app/src/components/Input-form.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="input-field">
+        <input type="text" id="InputBox" v-model='inputValue' @keyup.enter="saveItems" placeholder="Enter task here..."/>
+        <button @click="saveItems">ADD</button>
+    </div>
+</template>
+
+
+<script>
+   export default{
+     name:'InputForm',
+     data(){
+        return{
+         inputValue : '',
+        }
+     },
+     methods: {
+        saveItems() {
+            if(!this.inputValue){
+                alert('Please enter the input!')
+                return;
+            }
+
+            this.$store.commit('saveTask', this.inputValue);
+            this.inputValue = ''; 
+        },
+    },
+ 
+   }
+</script> 
+
+<style scoped>
+
+.input-field{
+    display:flex;
+    align-content: center;
+    justify-content: space-between;
+    gap:20px;
+}
+.input-field input{
+    width:90%;
+    height:60px;
+    border: none;
+    outline: none;
+    background-color:#919AB6;
+    border-radius: 20px;
+    font-size:18px;
+    padding:10px;
+    margin:10px 0px;
+    color:#fff;
+}
+.input-field input::placeholder{
+    padding:20px 40px;
+    font-size: 20px;
+    color:#fff;
+}
+.input-field button{
+   width:68px;
+   height: 60px;
+   padding:10px;
+   margin: 10px 0px;
+   border-radius: 10px;
+   cursor: pointer;
+   background-color:coral;
+   color:#fff;
+}
+.input-field button:hover{
+    background-color:rgb(240, 153, 122)  ;
+}
+.task-list ul.list-items{
+    min-height: 80px;
+    background-color: #6B799E;
+    border-radius: 30px;
+    color:#FFF;
+
+    padding:15px;
+    margin: 10px;
+    list-style: none;
+
+    display: flex;
+
+}
+.task-list ul li{
+    font-size: 1.4rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap:30px;
+}
+.task-list i{
+    cursor: pointer;
+}
+</style>
