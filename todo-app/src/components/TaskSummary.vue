0fd121d089c6d4a901fb373c00365579f683fdb3
Bhavyashree-DC <bhavyadc05@gmail.com>
2024-01-23 15:41:56 +0530

    Added task summary component

---
+++ todo-app/src/components/TaskSummary.vue
@@ -0,0 +1,35 @@
+ <template>
+    <div class="task-summary">
+        <div class="completed-summary">
+          Completed: {{ this.$store.getters.completedTask.length }}
+        </div>
+        <div class="pending-summary">
+          Pending: {{ this.$store.getters.pendingTask.length }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+   name:'TaskSummary',
+}
+</script>
+
+<style scoped>
+.task-summary {
+    display: flex;
+    justify-content: space-evenly;
+    gap:20px;
+    align-items: center;
+    margin: 10px 20px 0px 20px;
+  }
+  
+  .completed-summary,
+  .pending-summary {
+    border-bottom: 2px solid #007bff; 
+    border-radius: 20px;
+    background-color: rgb(113,127,166);
+    padding: 14px 18px;
+  }
+  
+</style>
